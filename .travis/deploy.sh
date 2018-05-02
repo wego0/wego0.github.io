9f0e92503d856b2e90bd224f0fa52d72f86af2b6
budaig <imovomi@gmail.com>
2018-05-02 13:54:51 +0800

    site update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,19 +2,23 @@
 
 # Decrypt the private key
 openssl aes-256-cbc -K $encrypted_6a5d0d2bb8ce_key -iv $encrypted_6a5d0d2bb8ce_iv -in .travis/4gcwgo.enc -out ~/.ssh/4gcwgo -d
+
 # Set the permission of the key
 chmod 600 ~/.ssh/4gcwgo
+
 # Start SSH agent
 eval $(ssh-agent)
+
 # Add the private key to the system
 ssh-add ~/.ssh/4gcwgo
+
 # Copy SSH config
 cp .travis/ssh_config ~/.ssh/config
 
 # Set Git config
-git init
 git config user.name "wego0"
 git config user.email "imovomi@sina.com"
+
 # Deploy to GitHub
 hexo deploy
 rm -rf ~/.ssh
